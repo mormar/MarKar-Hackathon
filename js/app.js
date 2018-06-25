093ada6068ff7695b6ce229c13e9f98f033836e5
Karol Rosiński <markar.toster@gmail.com>
2018-06-25 18:41:14 +0200

    Road

--- js/app.js
+++ js/app.js
@@ -1,19 +1,25 @@
-var map, infoWindow, marker;
+let map, infoWindow, directionsDisplay, pos, posTrue, markerPos;
+window.directionsService = new google.maps.DirectionsService();
+window.directionsDisplay = new google.maps.DirectionsRenderer();
 function initMap() {
+  let directionsService = new google.maps.DirectionsService();
+
+  directionsDisplay = new google.maps.DirectionsRenderer();
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: -34.397, lng: 150.644},
-    zoom: 18.5
+    zoom: 18.5,
+    mapTypeId: 'roadmap'
   });
   infoWindow = new google.maps.InfoWindow;
 
   // Try HTML5 geolocation.
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
-      var pos = {
+      pos = {
         lat: position.coords.latitude,
         lng: position.coords.longitude
       };
-
+      posTrue = new google.maps.LatLng(pos.lat, pos.lng);
       infoWindow.setPosition(pos);
       infoWindow.setContent('Location found.');
       infoWindow.open(map);
@@ -41,6 +47,19 @@ function initMap() {
     icon: 'img/bin24.png'
   });
   marker2.setMap(map);
+
+  directionsDisplay.setMap(map)
+  let travelMode = google.maps.TravelMode.WALKING;
+  let request = {
+    origin: pos,
+    destination: marker.position,
+    travelMode: travelMode
+  }
+  directionsService.route(request, function(response, status) {
+        if (status == google.maps.DirectionsStatus.OK) {
+            directionsDisplay.setDirections(response);
+        }
+    });
 }
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
